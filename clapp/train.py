f16898d8da4343dbc96ae245ba039d165ddfc834
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2022-12-25 11:20:22 +0000

    add learning rate scheduler

--- clapp/train.py
+++ clapp/train.py
@@ -6,9 +6,10 @@ from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.utils.data as data
 import torchvision.utils as VU
+import torch.utils.data as data
 import torchvision.transforms.functional as VF
+import torch.optim.lr_scheduler as lr_scheduler
 
 from clapp.model import FilterNet
 from clapp.data import ImageFilterStream
@@ -22,12 +23,14 @@ except ImportError:
 @dataclass
 class TrainConfig:
     device: str = "auto"
-    batch_size: int = 16
+    batch_size: int = 64
     num_workers: int = 0
-    num_iterations: int = 5000
+    num_iterations: int = 2500
     learning_rate: float = 1e-3
     output_dir: str = "outputs"
     output_log_interval: int = 100
+    min_lr: float = 1e-5
+    lr_cycle: int = 100
 
 
 def get_device(name: str):
@@ -60,6 +63,12 @@ class SimpleTrainer:
             self.model.parameters(),
             lr=config.learning_rate,
         )
+        self.scheduler = lr_scheduler.CosineAnnealingWarmRestarts(
+            self.optim,
+            T_0=config.lr_cycle,
+            T_mult=1,
+            eta_min=config.min_lr,
+        )
         self.loss_fn = nn.MSELoss()
 
         self.dataset = dataset
@@ -85,7 +94,8 @@ class SimpleTrainer:
             return
 
         targets = targets.expand(-1, 3, -1, -1)
-        outputs = outputs.expand(-1, 3, -1, -1).clamp(0, 1)  # fixes noisy image output bug
+        outputs = outputs.expand(-1, 3, -1, -1).clamp(0, 1)
+        # ^ fixes noisy image output bug
         grid = torch.stack([inputs[:count], targets[:count], outputs[:count]], dim=1)
         grid = grid.view(-1, *grid.shape[2:])
         grid = VU.make_grid(grid.cpu(), nrow=3)  # .cpu() because of pytorch/vision#6533
@@ -98,7 +108,14 @@ class SimpleTrainer:
     def log_loss(self, loss):
         self.progress.set_postfix(loss=loss)
         if wandb and wandb.run:
-            wandb.log({"loss": loss}, step=self.step_id)
+            lr = self.optim.param_groups[0]["lr"]
+            wandb.log(
+                {
+                    "loss": loss,
+                    "lr": lr,
+                },
+                step=self.step_id,
+            )
 
     def train_step(self, batch):
         self.step_id += 1
@@ -112,6 +129,7 @@ class SimpleTrainer:
         self.optim.zero_grad()
         loss.backward()
         self.optim.step()
+        self.scheduler.step()
 
         self.log_images(inputs, targets, outputs)
 
