2e11d3e4af9fa353119ef366eb4283f8f90b7e10
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2022-12-26 12:39:10 +0000

    add randomized filter

--- clapp/data.py
+++ clapp/data.py
@@ -1,6 +1,7 @@
 import time
 import random
 from threading import Thread
+from functools import partial
 from dataclasses import dataclass
 
 import cv2
@@ -9,9 +10,79 @@ import numpy as np
 from PIL import Image
 
 import torch
+import functorch
+import torch.nn as nn
 import torch.utils.data as data
 import torchvision.transforms as VT
 import torchvision.transforms.functional as VF
+
+
+def sobel_filter(image: Image.Image, blur=(3, 3), kernel=3):
+    img_np = np.array(image.convert("L"))
+    img_blur = cv2.GaussianBlur(img_np, blur, 0)
+    grad_x = cv2.Sobel(img_blur, cv2.CV_16S, 1, 0, ksize=kernel).astype(np.float32)
+    grad_y = cv2.Sobel(img_blur, cv2.CV_16S, 0, 1, ksize=kernel).astype(np.float32)
+    grad = np.sqrt(grad_x**2 + grad_y**2)
+    grad_norm = (grad * 255) / (grad.max() + 1e-8)
+    return grad_norm.astype(np.uint8)
+
+
+def sobel_canny_filter(image: Image.Image, blur=(3, 3), kernel=3, canny=(50, 150)):
+    sobel_grad = sobel_filter(image, blur, kernel)
+    return cv2.Canny(sobel_grad, *canny)
+
+
+def get_random_kernel(seed=None):
+    if not hasattr(get_random_kernel, f"kernel_{seed}"):
+        rnd = random.Random(seed)
+        ker = [rnd.random() for _ in range(27)]
+        ker = torch.tensor(ker).view(1, 3, 3, 3)
+        ker = ker / ker.sum()
+        mod = nn.Conv2d(3, 1, 3, padding=1, bias=False).requires_grad_(False)
+        mod.weight.data = ker
+        if seed:
+            setattr(get_random_kernel, f"kernel_{seed}", mod)
+        return mod
+    return getattr(get_random_kernel, f"kernel_{seed}")
+
+
+def random_filter(image: Image.Image, seed=None):
+    images = VF.to_tensor(image).unsqueeze(0)
+    kernel = get_random_kernel(seed)
+    output = kernel(images).clamp(0, 1)
+    return VF.to_pil_image(output[0])
+
+
+def random_p2_filter(image: Image.Image, seeds=(51, 15)):
+    a, b = seeds
+    if not hasattr(random_p2_filter, f"func_{a}_{b}"):
+        fmodel, params, buffers = functorch.combine_state_for_ensemble(
+            [
+                get_random_kernel(a),
+                get_random_kernel(b),
+            ]
+        )
+        func = partial(
+            functorch.vmap(fmodel, (0, 0, None)),
+            params,
+            buffers,
+        )
+        setattr(random_p2_filter, f"func_{a}_{b}", func)
+    else:
+        func = getattr(random_p2_filter, f"func_{a}_{b}")
+
+    images = VF.to_tensor(image).unsqueeze(0)
+    output = func(images).pow(2).sum(dim=0).sqrt()
+    output = output / (output.max() + 1e-8)
+    return VF.to_pil_image(output[0])
+
+
+target_filters = {
+    "sobel_3": partial(sobel_filter, kernel=3),
+    "sobel_canny_5": partial(sobel_canny_filter, kernel=5),
+    "random_41": partial(random_filter, seed=41),
+    "random2_51_15": partial(random_p2_filter, seeds=(51, 15)),
+}
 
 
 @dataclass
@@ -23,16 +94,7 @@ class DataConfig:
     resize_base: int = 96
     crop_size: int = 64
     image_key: str = "image"
-
-
-def sobel_filter(image: Image.Image, blur=(3, 3), kernel=3):
-    img_np = np.array(image.convert("L"))
-    img_blur = cv2.GaussianBlur(img_np, blur, 0)
-    grad_x = cv2.Sobel(img_blur, cv2.CV_16S, 1, 0, ksize=kernel).astype(np.float32)
-    grad_y = cv2.Sobel(img_blur, cv2.CV_16S, 0, 1, ksize=kernel).astype(np.float32)
-    grad = np.sqrt(grad_x**2 + grad_y**2)
-    grad_norm = (grad * 255) / grad.max()
-    return grad_norm.astype(np.uint8)
+    target_filter: str = "random2_51_15"
 
 
 class ImageFilterStream(data.IterableDataset):
@@ -46,7 +108,6 @@ class ImageFilterStream(data.IterableDataset):
             use_auth_token=True,
         )
         self._buffer = []
-        self._working = True
         self._min_buffer = config.min_buffer
         self._max_buffer = config.max_buffer
 
@@ -60,12 +121,12 @@ class ImageFilterStream(data.IterableDataset):
         while True:
             for x in self.dataset:
                 image = transform(x[self.config.image_key].convert("RGB"))
-                sobel = sobel_filter(image)
+                sobel = target_filters[self.config.target_filter](image)
                 image, sobel = map(VF.to_tensor, (image, sobel))
                 self._buffer.append((image, sobel))
                 if len(self._buffer) > self._max_buffer:
                     del self._buffer[0]
-                    time.sleep(0.01)
+                    time.sleep(0.1)
 
     def __iter__(self):
         Thread(
@@ -80,13 +141,8 @@ class ImageFilterStream(data.IterableDataset):
             torch.manual_seed(seed)
         rnd = random.Random(seed)
         # ---
-        self._working = True
-        while self._working:
+        while True:
             try:
                 yield rnd.choice(self._buffer)
             except:
                 break
-        self.stop()
-
-    def stop(self):
-        self._working = False
